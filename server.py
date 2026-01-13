d61e87c1c18fec102225c22b62d685e6a565eefe
d@tvn <157048397+dt97-K76@users.noreply.github.com>
2026-01-14 01:01:49 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,25 +1,21 @@
+import json
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
-import json, os, datetime
 
 app = FastAPI()
-
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/", response_class=HTMLResponse)
 async def index():
-    with open("static/index.html", "r", encoding="utf-8") as f:
-        return f.read()
+    return open("static/index.html", encoding="utf-8").read()
 
 @app.post("/collect")
 async def collect(request: Request):
     data = await request.json()
 
-    os.makedirs("logs", exist_ok=True)
-
-    filename = f"logs/{datetime.datetime.utcnow().isoformat()}.json"
-    with open(filename, "w") as f:
-        json.dump(data, f, indent=2)
+    print("==== NEW VISITOR ====")
+    print(json.dumps(data, indent=2))
+    print("=====================")
 
     return {"status": "ok"}
